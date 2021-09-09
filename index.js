05757598c7a69e3b376ad3d6889b8c12952d0163
lete114 <lete@lete114.top>
2021-09-09 09:56:53 +0800

    修复疏漏

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
  * Github: https://github.com/lete114/AWStats
  */
 
-const init = require('./lib/init')
 const generate = require('./lib/generated')
 const minify = require('./lib/minify')
 const server = require('./lib/server')
 const deploy = require('./lib/deploy')
 const clean = require('./lib/clean')
 
-module.exports = { init, generate, minify, server, deploy, clean }
+module.exports = { generate, minify, server, deploy, clean }
